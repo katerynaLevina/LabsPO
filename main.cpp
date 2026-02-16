f8bc6fc844e958798cbf30259d8f7602d43f70af
katerynaLevina <rg9a.klew@gmail.com>
2026-02-16 19:13:58 +0200

    + паралелізація

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <chrono>
-#include <random>
+#include <thread>
 
 using namespace std;
 
 const int N = 10000;
+const int numThreads = 16;
 
 int main() {
 
@@ -22,12 +23,27 @@ int main() {
         }
     }
 
+    thread* threads = new thread[numThreads];
+    int chunkSize = N / numThreads;
+
     auto start_time = chrono::high_resolution_clock::now();
 
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N - 1 - i; ++j) {
-            swap(matrix[i][j], matrix[N - 1 - j][N - 1 - i]);
-        }
+    for (int t = 0; t < numThreads; ++t) {
+        int start_row = t * chunkSize;
+        int end_row = (t == numThreads - 1) ? N : (t + 1) * chunkSize;
+
+        threads[t] = thread([matrix, start_row, end_row]() {
+            for (int i = start_row; i < end_row; ++i) {
+                int limit = N - 1 - i;
+                for (int j = 0; j < limit; ++j) {
+                    swap(matrix[i][j], matrix[N - 1 - j][N - 1 - i]);
+                }
+            }
+        });
+    }
+
+    for (int i = 0; i < numThreads; ++i) {
+        threads[i].join();
     }
 
     auto end_time = chrono::high_resolution_clock::now();
@@ -41,6 +57,7 @@ int main() {
         delete[] matrix[i];
     }
     delete[] matrix;
+    delete[] threads;
 
     return 0;
 }
